03d9e7edc5442b906092293aaeec08b57b766905
Aang Hermawan <jinguk13@gmail.com>
2025-09-19 10:33:50 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,10 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAyljFcBhoFr1wN0WXIOpiua5yKiahrpP0",
   authDomain: "workorder-app-c1b0f.firebaseapp.com",
   projectId: "workorder-app-c1b0f",
-  storageBucket: "workorder-app-c1b0f.appspot.com",
+  // Hapus baris ini:
+  // storageBucket: "workorder-app-c1b0f.appspot.com",
   messagingSenderId: "453685925986",
   appId: "1:453685925986:web:1c9a616c2a7c1251cd193b",
-  measurementId: "G-KYHSNQVD96"
+  // Hapus baris ini:
+  // measurementId: "G-KYHSNQVD96"
 };
